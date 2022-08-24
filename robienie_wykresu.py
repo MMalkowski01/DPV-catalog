e94ffd25ec4803637ed7e68870ae87509e4da5af
Intern <intern@amaltea.astrouw.edu.pl>
2022-08-24 16:08:30 +0200

    adding LMC,SMC and MWB markers in plot

--- robienie_wykresu.py
+++ robienie_wykresu.py
@@ -20,7 +20,7 @@ def otw_wynik(nazwa_pliku):
                 lista_P_orb.append(float(kolumna[2]))
                 lista_P_long.append(float(kolumna[3]))
         plik.close()
-        return (lista_rek,lista_dek,lista_P_orb,lista_P_long)
+        return (lista_rek,lista_dek,np.array(lista_P_orb),np.array(lista_P_long))
         
 rek, dek, P_orb, P_long = otw_wynik("wynik")
 catalog = SkyCoord(rek, dek, unit=(u.hourangle, u.deg))
@@ -28,16 +28,16 @@ MWB = SkyCoord("17:45:40.036", "-29:00:28.17",unit=(u.hourangle, u.deg))
 LMC = SkyCoord("05:23:34.0", "-69:45:24",unit=(u.hourangle, u.deg))
 SMC = SkyCoord("00:52:44.8", "-72:49:43",unit=(u.hourangle, u.deg))
 
-d_LMC = catalog.separation(LMC)
-d_SMC = catalog.separation(SMC)
-d_MWB = catalog.separation(MWB)
+d_LMC = catalog.separation(LMC).degree
+d_SMC = catalog.separation(SMC).degree
+d_MWB = catalog.separation(MWB).degree
 plt.figure(figsize=(10,7))
-#if d_LMC<5:
-#        plt.scatter(P_orb,P_long,c="firebrick",marker="v")
-#elif d_SMC<5:
-#        plt.scatter(P_orb,P_long,c="firebrick",marker="P")
-#elif d_MWB<5:
-plt.scatter(P_orb,P_long,c="firebrick",marker="P")
+maska_LMC = (d_LMC<15)
+maska_SMC = (d_SMC<5)
+maska_MWB = (d_MWB<15)
+plt.scatter(P_orb[maska_LMC],P_long[maska_LMC],c="firebrick",marker="v")  
+plt.scatter(P_orb[maska_SMC],P_long[maska_SMC],c="navy",marker="P")
+plt.scatter(P_orb[maska_MWB],P_long[maska_MWB],c="forestgreen",marker=".")
 plt.xlabel("P orb[d]")
 plt.ylabel("P long[d]")
 plt.title("P-P plot")
