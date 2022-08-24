5efdfd48a1b2bf835b4e32e694232a616747b0e5
Intern <intern@helike.astrouw.edu.pl>
2022-08-24 13:55:53 +0200

    added period

--- skycord_test.py
+++ skycord_test.py
@@ -20,16 +20,34 @@ def otw_C(nazwa_pliku):
         lista_id=[]
         lista_rek=[]
         lista_dek=[]
+        lista_per1=[]
+        lista_per2=[]
         for linia in plik:
                 kolumna = linia.split()
                 lista_id.append(kolumna[0])
                 lista_rek.append(kolumna[1])
                 lista_dek.append(kolumna[2])
+                lista_per1.append(kolumna[3])
+                lista_per2.append(kolumna[4])
         plik.close()
-        return (lista_id,lista_rek,lista_dek)
+        return (lista_id,lista_rek,lista_dek,lista_per1,lista_per2)
+
+def otw_Period_A(nazwa_pliku):
+        plik = open(nazwa_pliku,"r")
+        lista_id=[]
+        lista_per1=[]
+        lista_per2=[]
+        for linia in plik:
+                kolumna = linia.split()
+                lista_id.append(kolumna[0])
+                lista_per1.append(kolumna[3])
+                lista_per2.append(kolumna[7])
+        plik.close()
+        return (lista_id,lista_per1,lista_per2)
 
 plik_A=otw_A("2010AcA....60..179P")
 plik_C=otw_C("2003AA...399L..47M")
+plik_Per=otw_Period_A("2010AcA....60..179P_Period.dat")
 c = SkyCoord(plik_A[1], plik_A[2],unit=(u.hourangle, u.deg))
 catalog = SkyCoord(plik_C[1], plik_C[2],unit=(u.hourangle, u.deg))
 idxc, idxcatalog, d2d, _ = catalog.search_around_sky(c, 2*u.arcsec)
@@ -38,7 +56,7 @@ out = open("wynik","w")
 numerek = 0
 usuniete_C =[]
 for i,linia in enumerate(plik_A[0]):
-        tekst = plik_A[1][i]+" "+plik_A[2][i]+" "+plik_A[0][i]+" "
+        tekst = plik_A[1][i]+" "+plik_A[2][i]+" "+plik_Per[1][i]+" "+plik_Per[2][i]+" "+plik_A[0][i]+" "
         if i in idxc:
                 IDC = plik_C[0][idxcatalog[numerek]]
                 tekst += IDC
@@ -50,7 +68,7 @@ for i,linia in enumerate(plik_A[0]):
 for i,linia in enumerate(plik_C[0]):
         if plik_C[0][i] in usuniete_C:
                 continue
-        tekst = plik_C[1][i]+" "+plik_C[2][i]+" "+"-               "+" "+plik_C[0][i]
+        tekst = plik_C[1][i]+" "+plik_C[2][i]+" "+plik_C[3][i]+" "+plik_C[4][i]+" "+"-               "+" "+plik_C[0][i]
         print(tekst,file=out)        
 out.close()
 
